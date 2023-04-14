618113704a54e2883de7cab29599a362692fa425
cpeters5 <cpeters5@yahoo.com>
2023-04-14 19:07:11 +0000

    includ common.pl file

--- scripts/cron/updateancdesc.pl
+++ scripts/cron/updateancdesc.pl
@@ -9,25 +9,28 @@
 use strict;
 use warnings FATAL => 'all';
 use DBI;
-my $host = '134.209.46.210';
+require "common.pl";
+our ($sth, $sth1);
+
+ # my $host = '134.209.46.210';
 my $db = "bluenanta";
 # Database connection
-my $dbh = DBI->connect( "DBI:mysql:$db:$host","chariya","Imh#r3r3") or die( "Could not connect to: $DBI::errstr" );
+# my $dbh = DBI->connect( "DBI:mysql:$db:$host","chariya","Imh#r3r3") or die( "Could not connect to: $DBI::errstr" );
 # $dbh = DBI->connect( "DBI:mysql:$db:localhost","chariya","imh3r3r3") or die( "Could not connect to: $DBI::errstr" );
-my ($sth, $sth1);
 &getASPM("use $db");
 
 if ($ARGV[0]) {
-        my $pid = $ARGV[0];
-        my $stmt = "select seed_id, pollen_id from orchidaceae_hybrid where pid = ?";
-        my @row = $dbh->selectrow_array($stmt,undef,$pid);
-        unless (@row) { die "Pid $pid not found in Hybrid"; }
-        my ($seed,$poll) = @row;
-        print "$pid, $seed, $poll\n";
-        updatedata($pid,$seed,$poll);
-        exit;
+	my $pid = $ARGV[0];
+	my $stmt = "select seed_id, pollen_id from orchidaceae_hybrid where pid = $pid";
+	&getASPM($stmt);
+	while (my @row = $sth->fetchrow_array()) {
+		unless (@row) {die "Pid $pid not found in Hybrid";}
+		my ($seed, $poll) = @row;
+		print "$pid, $seed, $poll\n";
+		updatedata($pid, $seed, $poll);
+		exit;
+	}
 }
-
 my $debug = 0;
 my %pid  = ();
 my %seed = ();
@@ -38,17 +41,6 @@ my $totalpair = 0;
 use Math::Round;
 # Refresh ancdesc for inpout pid
 
-if ($ARGV[0]) {
-	my $pid = $ARGV[0];
-	my $stmt = "select seed_id, pollen_id from orchidaceae_hybrid where pid = ?";
-	my @row = $dbh->selectrow_array($stmt,undef,$pid);
-	unless (@row) { die "Pid $pid not found in Hybrid"; }
-	my ($seed,$poll) = @row;
-	print "$pid, $seed, $poll\n";
-	updatedata($pid,$seed,$poll);
-	exit;
-}
-
 &initHybrid();
 
 foreach my $pid (sort keys %pid) {
@@ -58,7 +50,8 @@ foreach my $pid (sort keys %pid) {
 }
 
 sub initHybrid {
-    my $stmt = "select pid, seed_id, pollen_id from orchidaceae_hybrid where pid not in (select distinct did from orchidaceae_ancestordescendant)
+    my $stmt = "select pid, seed_id, pollen_id from orchidaceae_hybrid where pid not in
+    			(select distinct did from orchidaceae_ancestordescendant)";
     &getASPM($stmt);
     while (my @row = $sth->fetchrow_array()) {
         $pid{$row[0]}++;
@@ -93,16 +86,4 @@ sub updatedata {
 		# print "$totalpair\t$numberpair\t$aid{$aid}\t$aid\t$pid\n";
 		&getASPM1($stmt);
 	}
-}
-
-sub getASPM {
-	my $stmt = shift;
-	$sth = $dbh->prepare( $stmt ) or die( "\n$stmt\nCannot prepare: ", $dbh->errstr(), "\n" );
-	my $rc = $sth->execute() or die("\nDead! \n$stmt\nCannot execute: ", $sth->errstr(),"\n" );
-}
-
-sub getASPM1 {
-	my $stmt = shift;
-	$sth1 = $dbh->prepare( $stmt ) or die( "\n$stmt\nCannot prepare: ", $dbh->errstr(), "\n" );
-	my $rc = $sth1->execute() or die("\nDead! \n$stmt\nCannot execute: ", $sth1->errstr(),"\n" );
 }
