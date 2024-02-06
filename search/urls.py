c7a98b7051677beb41dbf1f9f12185577ced2c91
cpeters5 <cpeters5@yahoo.com>
2024-02-06 16:39:24 +0000

    cleanup code

--- search/urls.py
+++ search/urls.py
@@ -8,13 +8,7 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('search_species/', views.search_species, name='search_species'),
     path('search_name/', views.search_name, name='search_name'),
-
-    # Local search: Based on family(families) found, search for matching species
-    # path('search_orchid/', views.search_orchid, name='search_orchid'),
-    # path('search_other/', views.search_other, name='search_other'),
-    # path('search_fungi/', views.search_fungi, name='search_fungi'),
-    # path('search_aves/', views.search_aves, name='search_aves'),
-    # path('search_animalia/', views.search_animalia, name='search_animalia'),
+    # path('search_distribution/', views.search_distribution, name='search_distribution'),
     path('search_fuzzy/', views.search_fuzzy, name='search_fuzzy'),
 
     # If neither family nor application, nor genus is given, then search across all tables for matching species
