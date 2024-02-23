460fcda2e111861aa6753f9995ef998bc5067cf5
cpeters5 <cpeters5@yahoo.com>
2024-02-23 19:53:45 +0000

    Fixing several bugs

--- scripts/cron/generate_thumbs.py
+++ scripts/cron/generate_thumbs.py
@@ -9,37 +9,46 @@ start_time = time.time()
 def generate_thumbnails(base_path):
     images_path = os.path.join(base_path, 'images/')
     thumb_path = os.path.join(base_path, 'thumbs/')
+    thumb_size = 300
     i = 0
+    j = 0
     flag = False
     for subdir, dirs, files in os.walk(images_path):
-        # print(i, subdir)
         for file in files:
             if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
-                try:
-                    # Create a path for the original file and the thumbnail
-                    original_file_path = os.path.join(subdir, file)
-                    thumb_subdir = subdir.replace(images_path, thumb_path)
-                    if not os.path.exists(thumb_subdir):
-                        os.makedirs(thumb_subdir)
-                    thumb_file_path = os.path.join(thumb_subdir, file)
+                j += 1
+                # Create a path for the original file and the thumbnail
+                original_file_path = os.path.join(subdir, file)
+                thumb_subdir = subdir.replace(images_path, thumb_path)
+                if not os.path.exists(thumb_subdir):
+                    os.makedirs(thumb_subdir)
+                thumb_file_path = os.path.join(thumb_subdir, file)
 
-                    # Check if thumbnail already exists
-                    if not os.path.exists(thumb_file_path):
-                        # Open the image and convert it to a thumbnail
-                        with Image.open(original_file_path) as img:
-                            img.thumbnail((128, 128))
+                # Check if thumbnail already exists
+                if not os.path.exists(thumb_file_path):
+                    # Open the image and convert it to a thumbnail
+                    try:
+                        img = Image.open(original_file_path)
+                        try:
+                            img.thumbnail((thumb_size, thumb_size))
                             img.save(thumb_file_path)
-                        logging.info(f"Thumbnail created for {original_file_path}")
-                    else:
-                        # logging.info(f"Thumbnail already exists for {original_file_path}")
-                        continue
-                except IOError:
-                    i += 1
-                    # logging.error(f"{i} - Cannot create thumbnail for {original_file_path}")
-        #             if i == 2000:
-        #                 flag = True
-        # if flag:
-        #     break
+                        except:
+                            # If file corrupted, use original
+                            img.save(thumb_file_path)
+                            logging.info(f"Cant create Thumbnail. Use original {original_file_path}")
+                        if j%100 == 0:
+                            logging.info(f"{j} - Thumbnail created for {original_file_path}")
+                    except IOError:
+                        logging.info(f"{i} - Unable to identify {original_file_path}")
+                        i += 1
+                        if i == 2000:
+                            flag = True
+
+                else:
+                    # logging.info(f"Thumbnail already exists for {original_file_path}")
+                    pass
+        if flag:
+            break
     return i
 
 # Setup basic logging
@@ -50,4 +59,4 @@ i = generate_thumbnails('/mnt/static/utils/')
 end_time = time.time()
 duration = end_time - start_time
 
-print(f"Fail to create thumbnails for {i} images. The process took {duration} seconds.")
+print(f"\nFail to create thumbnails for {i} images. The process took {duration} seconds.")
