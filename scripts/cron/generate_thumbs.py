70add1d001b181785be5bde93ff676c698e14220
cpeters5 <cpeters5@yahoo.com>
2024-03-05 16:22:51 +0000

    implemented thumb nail generator

--- scripts/cron/generate_thumbs.py
+++ scripts/cron/generate_thumbs.py
@@ -1,5 +1,6 @@
 import os
 import time
+import shutil  # For copying files
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = 933120000
 import logging
@@ -16,7 +17,6 @@ def generate_thumbnails(base_path):
     for subdir, dirs, files in os.walk(images_path):
         for file in files:
             if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
-                j += 1
                 # Create a path for the original file and the thumbnail
                 original_file_path = os.path.join(subdir, file)
                 thumb_subdir = subdir.replace(images_path, thumb_path)
@@ -38,7 +38,9 @@ def generate_thumbnails(base_path):
                             logging.info(f"Cant create Thumbnail. Use original {original_file_path}")
                         if j%100 == 0:
                             logging.info(f"{j} - Thumbnail created for {original_file_path}")
+                        j += 1
                     except IOError:
+                        shutil.copy(original_file_path, thumb_file_path)
                         logging.info(f"{i} - Unable to identify {original_file_path}")
                         i += 1
                         if i == 2000:
@@ -49,14 +51,14 @@ def generate_thumbnails(base_path):
                     pass
         if flag:
             break
-    return i
+    return i, j
 
 # Setup basic logging
 logging.basicConfig(level=logging.INFO)
 
 # Assuming the base directory is the current working directory
-i = generate_thumbnails('/mnt/static/utils/')
+i, j = generate_thumbnails('/mnt/static/utils/')
 end_time = time.time()
 duration = end_time - start_time
 
-print(f"\nFail to create thumbnails for {i} images. The process took {duration} seconds.")
+print(f"\nCreated {j} thumbnailes. Fail to create {i}. The process took {duration} seconds.")
