78dd710e863bd3346e167785b5840112223039aa
cpeters5 <cpeters5@yahoo.com>
2024-03-05 16:21:09 +0000

    move core app to common

--- scripts/gen_taxonomy.py
+++ scripts/gen_taxonomy.py
@@ -11,30 +11,30 @@ cur = conn.cursor()
 curinsert = conn.cursor()
 
 # Family
-stmt = "insert ignore into core_taxonomy (taxon, `rank`, `level`) SELECT family, 'family', 1 from core_family;"
+stmt = "insert ignore into common_taxonomy (taxon, `rank`, `level`) SELECT family, 'family', 1 from common_family;"
 curinsert.execute(stmt)
 conn.commit()
 
 # SubFamily
-stmt = "insert ignore into core_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.subfamily, 'subfamily', b.level + 1, b.id, b.taxon from core_subfamily a join core_taxonomy b on a.family = b.taxon;"
+stmt = "insert ignore into common_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.subfamily, 'subfamily', b.level + 1, b.id, b.taxon from common_subfamily a join common_taxonomy b on a.family = b.taxon;"
 curinsert.execute(stmt)
 conn.commit()
 
 # Tribe
-stmt = "insert ignore into core_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.tribe, 'tribe', b.level + 1, b.id, b.taxon from core_tribe a join core_taxonomy b on a.subfamily = b.taxon;"
+stmt = "insert ignore into common_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.tribe, 'tribe', b.level + 1, b.id, b.taxon from common_tribe a join common_taxonomy b on a.subfamily = b.taxon;"
 curinsert.execute(stmt)
 conn.commit()
-stmt = "insert ignore into core_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.tribe, 'tribe', b.level + 1, b.id, b.taxon from core_tribe a join core_taxonomy b on a.family = b.taxon and a.subfamily is null or a.subfamily = '';"
+stmt = "insert ignore into common_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.tribe, 'tribe', b.level + 1, b.id, b.taxon from common_tribe a join common_taxonomy b on a.family = b.taxon and a.subfamily is null or a.subfamily = '';"
 curinsert.execute(stmt)
 conn.commit()
 
 # Subtribe
-stmt = "insert ignore into core_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.subtribe, 'subtribe', b.level + 1, b.id, b.taxon from core_subtribe a join core_taxonomy b on a.tribe = b.taxon;"
+stmt = "insert ignore into common_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.subtribe, 'subtribe', b.level + 1, b.id, b.taxon from common_subtribe a join common_taxonomy b on a.tribe = b.taxon;"
 curinsert.execute(stmt)
 conn.commit()
-stmt = "insert ignore into core_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.subtribe, 'subtribe', b.level + 1, b.id, b.taxon from core_subtribe a join core_taxonomy b on a.subfamily = b.taxon and a.tribe is null or a.tribe = '';"
+stmt = "insert ignore into common_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.subtribe, 'subtribe', b.level + 1, b.id, b.taxon from common_subtribe a join common_taxonomy b on a.subfamily = b.taxon and a.tribe is null or a.tribe = '';"
 curinsert.execute(stmt)
 conn.commit()
-stmt = "insert ignore into core_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.subtribe, 'subtribe', b.level + 1, b.id, b.taxon from core_subtribe a join core_taxonomy b on a.family = b.taxon and (a.subfamily is null or a.subfamily = '') and (a.tribe is null or a.tribe = '');"
+stmt = "insert ignore into common_taxonomy (taxon, `rank`, `level`, parent_id, parent_name) SELECT a.subtribe, 'subtribe', b.level + 1, b.id, b.taxon from common_subtribe a join common_taxonomy b on a.family = b.taxon and (a.subfamily is null or a.subfamily = '') and (a.tribe is null or a.tribe = '');"
 curinsert.execute(stmt)
 conn.commit()
