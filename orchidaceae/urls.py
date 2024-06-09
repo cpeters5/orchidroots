0e271e7ec8dde6672dbfe8dfd2a682c1b3aa60bd
cpeters5 <cpeters5@yahoo.com>
2024-06-09 08:46:17 -0400

    Cleanup path redirection. The right way!

--- orchidaceae/urls.py
+++ orchidaceae/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, re_path
 from . import views
 from display.views import photos as display_photos, information as display_information
-from detail.views import reidentify as detail_reidentify, uploadfile as detail_uploadfile, \
-    uploadweb as detail_uploadweb, curateinfospc as detail_curateinfospc, curateinfohyb as detail_curateinfohyb, \
-    compare as detail_compare, createhybrid as detail_createhybrid, approvemediaphoto as detail_approvemediaphoto
+from detail.views import reidentify, uploadfile, uploadweb, curateinfospc, curateinfohyb, \
+    compare, createhybrid, approvemediaphoto
 
 
 app_name = 'orchidaceae'
@@ -16,32 +15,11 @@ urlpatterns = [
     path('ancestor/', views.ancestor, name='ancestor'),
     path('ancestor/<int:pid>/', views.ancestor, name='ancestor'),
     path('ancestrytree/<int:pid>/', views.ancestrytree, name='ancestrytree'),
-    path('ancestrytree/', views.ancestrytree, name='ancestrytree'),
     path('progeny/<int:pid>/', views.progeny, name='progeny'),
     path('progenyimg/<int:pid>/', views.progenyimg, name='progenyimg'),
-    path('progenyimg/', views.progenyimg, name='progenyimg'),
     path('subgenus/', views.subgenus, name='subgenus'),
     path('section/', views.section, name='section'),
     path('subsection/', views.subsection, name='subsection'),
     path('series/', views.series, name='series'),
-    # path('browsegen/', views.browsegen, name='browsegen'),
-
-    # Redirect to display
-    path('information/', display_information, name='information'),
-    path('information/<int:pid>/', display_information, name='information'),
-    path('photos/', display_photos, name='photos'),
-    path('photos/<int:pid>/', display_photos, name='photos'),
-
-    # Redirect to detail
-    path('reidentify/<int:orid>/<int:pid>/', detail_reidentify, name='reidentify'),
-    path('uploadweb/<int:pid>/', detail_uploadweb, name='uploadweb'),
-    path('uploadweb/<int:pid>/<int:orid>/', detail_uploadweb, name='uploadweb'),
-    path('uploadfile/<int:pid>/', detail_uploadfile, name='uploadfile'),
-    path('approvemediaphoto/<int:pid>/', detail_approvemediaphoto, name='approvemediaphoto'),
-
-    path('curateinfospc/<int:pid>/', detail_curateinfospc, name='curateinfospc'),
-    path('curateinfohyb/<int:pid>/', detail_curateinfohyb, name='curateinfohyb'),
-    path('compare/<int:pid>/', detail_compare, name='compare'),
-    path('createhybrid/', detail_createhybrid, name='createhybrid'),
 
 ]
