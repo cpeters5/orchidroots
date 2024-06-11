4c0b37703d69e8472f9af69ef2f84106e27b3a3d
cpeters5 <cpeters5@yahoo.com>
2024-06-11 15:01:44 -0400

    Adding big_genera list

--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,15 @@
 import string
 alpha_list = string.ascii_uppercase
 applications = ['orchidaceae', 'other', 'fungi', 'aves', 'animalia']
+big_genera = [
+    'Phalaenopsis',
+    'Paphiopedilum',
+    'Cattleya',
+    'Dendrobium',
+    'Cymbidium',
+    'Rhyncholaeliocattleya',
+    'Oncidium',
+    'Laeliocattleya',
+    'Brassolaeliocattleya',
+    'Doritaenopsis'
+]
