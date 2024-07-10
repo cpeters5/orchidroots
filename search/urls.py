cfaa0981fee5bae4f310fb93e6987505406a3b7d
cpeters5 <cpeters5@yahoo.com>
2024-07-09 22:11:24 -0400

    Add search test path

--- search/urls.py
+++ search/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('search_species/', views.search_species, name='search_species'),
     path('search_name/', views.search_name, name='search_name'),
-    path('search_binomial/', views.search_binomial, name='search_binomial'),
+    path('search_test/', views.search_test, name='search_test'),
     path('search_orchidaceae/', views.search_orchidaceae, name='search_orchidaceae'),
 
     # If neither family nor application, nor genus is given, then search across all tables for matching species
