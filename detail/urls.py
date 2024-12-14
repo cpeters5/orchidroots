7b2247f3fbe56d3973dd1dc14e65592ff97b90c5
cpeters5 <cpeters5@yahoo.com>
2024-12-13 19:11:02 -0500

    Adding login required to costly urls to prevent crawlers accessing them

--- detail/urls.py
+++ detail/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 from orchidaceae.views import ancestor as orchidaceae_ancestor, ancestrytree as orchidaceae_ancestrytree, \
     progeny as orchidaceae_progeny, progenyimg as orchidaceae_progenyimg
@@ -14,22 +15,22 @@ urlpatterns = [
     # detail
     path('compare/<int:pid>/', views.compare, name='compare'),
     path('compare/', views.compare, name='compare'),
-    path('createhybrid/', views.createhybrid, name='createhybrid'),
-    path('comment/', views.comment, name='comment'),
-    path('comments/', views.comments, name='comments'),
-    path('curateinfospc/<int:pid>/', views.curateinfospc, name='curateinfospc'),
-    path('curateinfospc/', views.curateinfospc, name='curateinfospc'),
-    path('curateinfohyb/<int:pid>/', views.curateinfohyb, name='curateinfohyb'),
-    path('curateinfohyb/', views.curateinfohyb, name='curateinfohyb'),
-    path('reidentify/<int:orid>/<int:pid>/', views.reidentify, name='reidentify'),
-    path('uploadweb/<int:pid>/', views.uploadweb, name='uploadweb'),
-    path('uploadweb/<int:pid>/<int:orid>/', views.uploadweb, name='uploadweb'),
-    path('uploadfile/<int:pid>/', views.uploadfile, name='uploadfile'),
+    path('createhybrid/', login_required(views.createhybrid), name='createhybrid'),
+    path('comment/', login_required(views.comment), name='comment'),
+    path('comments/', login_required(views.comments), name='comments'),
+    path('curateinfospc/<int:pid>/', login_required(views.curateinfospc), name='curateinfospc'),
+    path('curateinfospc/', login_required(views.curateinfospc), name='curateinfospc'),
+    path('curateinfohyb/<int:pid>/', login_required(views.curateinfohyb), name='curateinfohyb'),
+    path('curateinfohyb/', login_required(views.curateinfohyb), name='curateinfohyb'),
+    path('reidentify/<int:orid>/<int:pid>/', login_required(views.reidentify), name='reidentify'),
+    path('uploadweb/<int:pid>/', login_required(views.uploadweb), name='uploadweb'),
+    path('uploadweb/<int:pid>/<int:orid>/', login_required(views.uploadweb), name='uploadweb'),
+    path('uploadfile/<int:pid>/', login_required(views.uploadfile), name='uploadfile'),
 
     # Legacy urls
     path('information/', views.information, name='redirect_information'),
     path('photos/', views.photos, name='redirect_photos'),
-    path('ancestor/', views.ancestor, name='redirect_ancestor'),
-    path('ancestrytree/', views.ancestrytree, name='redirect_ancestrytree'),
+    path('ancestor/', login_required(views.ancestor), name='redirect_ancestor'),
+    path('ancestrytree/', login_required(views.ancestrytree), name='redirect_ancestrytree'),
 
 ]
--- orchidaceae/urls.py
+++ orchidaceae/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
+from django.contrib.auth.decorators import login_required
 from . import views
-from detail.views import reidentify as detail_reidentify, uploadfile as detail_uploadfile, \
+from detail.views import (reidentify as detail_reidentify, uploadfile as detail_uploadfile, \
     uploadweb as detail_uploadweb, curateinfospc as detail_curateinfospc, curateinfohyb as detail_curateinfohyb, \
-    compare as detail_compare, createhybrid as detail_createhybrid
+    compare as detail_compare, createhybrid as detail_createhybrid)
+#, approvemediaphoto as detail_approvemediaphoto)
 
 
 app_name = 'orchidaceae'
@@ -10,16 +12,16 @@ urlpatterns = [
     # Top level
     path('', views.genera, name='genera'),
     path('genera/', views.genera, name='genera'),
-    path('species/', views.species, name='species'),
-    path('hybrid/', views.hybrid, name='hybrid'),
-    path('ancestor/', views.ancestor, name='ancestor'),
-    path('ancestor/<int:pid>/', views.ancestor, name='ancestor'),
-    path('ancestrytree/<int:pid>/', views.ancestrytree, name='ancestrytree'),
-    path('ancestrytree/', views.ancestrytree, name='ancestrytree'),
+    path('species/', login_required(views.species), name='species'),
+    path('hybrid/', login_required(views.hybrid), name='hybrid'),
+    path('ancestor/', login_required(views.ancestor), name='ancestor'),
+    path('ancestor/<int:pid>/', login_required(views.ancestor), name='ancestor'),
+    path('ancestrytree/<int:pid>/', login_required(views.ancestrytree), name='ancestrytree'),
+    path('ancestrytree/', login_required(views.ancestrytree), name='ancestrytree'),
     path('synonym/<int:pid>/', views.synonym, name='synonym'),
-    path('progeny/<int:pid>/', views.progeny, name='progeny'),
-    path('progenyimg/<int:pid>/', views.progenyimg, name='progenyimg'),
-    path('progenyimg/', views.progenyimg, name='progenyimg'),
+    path('progeny/<int:pid>/', login_required(views.progeny), name='progeny'),
+    path('progenyimg/<int:pid>/', login_required(views.progenyimg), name='progenyimg'),
+    path('progenyimg/', login_required(views.progenyimg), name='progenyimg'),
     path('subgenus/', views.subgenus, name='subgenus'),
     path('section/', views.section, name='section'),
     path('subsection/', views.subsection, name='subsection'),
@@ -27,15 +29,17 @@ urlpatterns = [
     # path('browsegen/', views.browsegen, name='browsegen'),
 
     # Redirect to detail
-    path('reidentify/<int:orid>/<int:pid>/', detail_reidentify, name='reidentify'),
-    path('uploadweb/<int:pid>/', detail_uploadweb, name='uploadweb'),
-    path('uploadweb/<int:pid>/<int:orid>/', detail_uploadweb, name='uploadweb'),
-    path('uploadfile/<int:pid>/', detail_uploadfile, name='uploadfile'),
+    path('reidentify/<int:orid>/<int:pid>/', login_required(detail_reidentify), name='reidentify'),
+    path('uploadweb/<int:pid>/', login_required(detail_uploadweb), name='uploadweb'),
+    path('uploadweb/<int:pid>/<int:orid>/', login_required(detail_uploadweb), name='uploadweb'),
+    path('uploadfile/<int:pid>/', login_required(detail_uploadfile), name='uploadfile'),
+    # path('approvemediaphoto/<int:pid>/', detail_approvemediaphoto, name='approvemediaphoto'),
 
-    path('curateinfospc/<int:pid>/', detail_curateinfospc, name='curateinfospc'),
-    path('curateinfohyb/<int:pid>/', detail_curateinfohyb, name='curateinfohyb'),
+    path('curateinfospc/<int:pid>/', login_required(detail_curateinfospc), name='curateinfospc'),
+    path('curateinfohyb/<int:pid>/', login_required(detail_curateinfohyb), name='curateinfohyb'),
     path('compare/<int:pid>/', detail_compare, name='compare'),
-    path('createhybrid/', detail_createhybrid, name='createhybrid'),
+    path('createhybrid/', login_required(detail_createhybrid), name='createhybrid'),
+
 
     # Transferredf to common
     path('infraspecific/<int:pid>/', views.infraspecific, name='infraspecific'),
